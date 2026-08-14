f069975030c3dd96677462114a0fa6aa2d393fd6
adarshsankeshwar <adarshsankeshwar@gmail.com>
2026-08-14 09:17:41 +0530

    Addded 1st Hibernate Example

--- FirstHibernate/src/main/java/com/kodnest/FirstHibernate/App.java
+++ FirstHibernate/src/main/java/com/kodnest/FirstHibernate/App.java
@@ -1,13 +1,34 @@
 package com.kodnest.FirstHibernate;
 
-/**
- * Hello world!
- *
- */
+import java.util.*;
+
+import org.hibernate.Session;
+import org.hibernate.SessionFactory;
+import org.hibernate.Transaction;
+import org.hibernate.cfg.Configuration;
 public class App 
 {
     public static void main( String[] args )
     {
-        System.out.println( "Hello World!" );
+        Scanner sc = new Scanner(System.in);
+        System.out.println("Enter the name, gender, salary");
+        Employee emp = new Employee(sc.next(), sc.next(), sc.nextInt());
+        
+        Configuration conf = new Configuration();
+        conf.configure("hibernate.cfc.xml");
+        
+        SessionFactory factory = conf.buildSessionFactory();
+        
+        Session session = factory.openSession();
+        
+        Transaction transaction = session.beginTransaction();
+        
+        session.persist(emp);
+        
+        transaction.commit();
+        
+        session.close();
+        factory.close();
+        
     }
 }
